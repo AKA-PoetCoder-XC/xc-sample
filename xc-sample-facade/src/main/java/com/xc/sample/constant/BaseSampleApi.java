d27b25a199d34ee203ec9d4cf382c9576e875693
xiechen <xiechen@jiayihn.net>
2024-12-03 18:04:28 +0800

    xc-sample项目初始化

---
+++ xc-sample-facade/src/main/java/com/xc/sample/constant/BaseSampleApi.java
@@ -0,0 +1,14 @@
+package com.xc.sample.constant;
+
+/**
+ * api常量
+ *
+ * @description 给api调用方定义的常量放在这里，命名前部分为工程名称，方便调用方区分
+ * @author XieChen
+ * @date 2024-12-03
+ */
+public interface BaseSampleApi {
+
+	String APPLICATION_NAME = "sample";
+
+}
---
+++ xc-sample-service/src/main/java/com/xc/sample/SampleApplication.java
@@ -0,0 +1,29 @@
+package com.xc.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootApplication
+@EnableFeignClients
+public class SampleApplication {
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext application = SpringApplication.run(SampleApplication.class, args);
+        Environment env = application.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = env.getProperty("server.port");
+        String path = env.getProperty("server.servlet.context-path");
+        log.info("\n------------------------------------------------------------\n"
+                + "Application is running!\n"
+                + "Swagger文档: http://" + ip + ":" + port + path + "/doc.html\n"
+                + "------------------------------------------------------------");
+
+    }
+}
