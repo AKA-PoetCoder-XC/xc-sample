941b664028cd0c7b9232c81bb7897c8b9d04e5c9
xiechen <xiechen@jiayihn.net>
2025-04-26 15:26:57 +0800

    集成knife4j-openapi3-jakarta-spring-boot-starter依赖，自动生成swagger文档

--- xc-sample-service/src/main/java/com/xc/sample/controller/SampleController.java
+++ xc-sample-service/src/main/java/com/xc/sample/controller/SampleController.java
@@ -1,15 +1,19 @@
 package com.xc.sample.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "示例控制器")
 @RestController
 @RequestMapping(("/sample"))
 public class SampleController {
 
-    @GetMapping("/hello")
+    @GetMapping("/sample")
+    @Operation(summary = "示例接口")
     public String hello() {
-        return "Hello World";
+        return "Hello,this is sample controller";
     }
 }
